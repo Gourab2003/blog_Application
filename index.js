38144cf2cc3e7975903224790adf72dfc486cb65
Gourab <roygourab005@gmail.com>
2024-02-23 19:42:35 +0530

    update the code base based on mongosh

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 import  express from "express";
-import bodyParser from "body-parser";
 import mongoose, {Schema} from "mongoose";
-import fs from "fs"
-import { config } from "process";
 
 const app = express();
 const port = 3000;
+
+
 // mongodb connections
 mongoose.connect("mongodb://localhost:27017/Gourab")
 .then(()=>{
@@ -14,7 +13,9 @@ mongoose.connect("mongodb://localhost:27017/Gourab")
     console.log(err.message, "connection failed");
 })
 
-app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.use(express.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 const blogSchema = new Schema({
@@ -44,7 +45,7 @@ const commentsSchema = new Schema({
         type: String, 
         required: true
     },
-    comments:{
+    comment:{
         type: String,
         required: true,
     }
@@ -52,25 +53,20 @@ const commentsSchema = new Schema({
 
 const Comments = mongoose.model("Comments",commentsSchema);
 
-let Data;
 
-fs.readFile('post.json', 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading post.json:', err);
-        return;
+app.get("/",async(req, res)=>{
+    try {
+        const blogPost = await Blog.find({});
+        res.status(200).render("index.ejs", {Blog:blogPost});
+    } catch (error) {
+        console.log(error.message);
     }
-     Data = JSON.parse(data);
-
-});
-
-app.get("/",(req, res)=>{
-    res.render("index.ejs", {posts:Data} );
 });
 
 
 app.get('/post/:id', (req, res) => {
     const postId = req.params.id;
-    const post = Data.find(post => post.id == postId);
+    const post = Blog.find(post => post.id == postId);
     if (!post) {
         res.status(404).send('Post not found');
     } else {
@@ -82,49 +78,32 @@ app.get("/contact",(req,res)=>{
     res.render("contact.ejs");
 });
 
-app.post("/submit", (req, res) => {
-    const name = req.body.name;
-    const email = req.body.email;
-    const comment = req.body.text;
-    const data = `\nName:${name}\nEmail:${email}\nComment:${comment}\n`
-    fs.appendFile("contact.txt",data, (err)=>{
-            if(err){
-                res.end(`<h1>Something went wrong, please restart the server</h1>`)
-            }
-            else{
-                res.redirect("/")
-            }
-    })
+app.post("/submit",  async(req, res) => { 
+   const result = await Comments.create({
+        name: req.body.name,
+        email: req.body.email,
+        comment: req.body.comment
+    });
+
+    console.log(result);
+
+    return res.status(200).redirect("index.ejs")
 });
 
-app.get("/createBlog",(req, res)=>{
+app.get("/createBlog",(req, res)=>{ 
     res.render("create_blog.ejs")
 });
 
-app.post('/createBlog', (req, res) => {
-    const title = req.body.title;
-    const author = req.body.author;
-    const content = req.body.content;
-    const currentDate = new Date().toISOString().slice(0, 10);
+app.post('/createBlog', async(req, res) => {
+   const blogs = await Blog.create({
+    title : req.body.title,
+    author: req.body.author,
+    content: req.body.content,
+    time: new Date()
+   });
+   console.log(blogs);
 
-    let Data = [];
-    try {
-        Data = JSON.parse(fs.readFileSync('post.json'));
-    } catch (error) {
-        console.error('Error reading posts.json:', error);
-    }
-    const id = Data.length + 1;
-    const newPost = {
-        id,
-        title,
-        author,
-        date: currentDate,
-        content
-    };
-
-    Data.push(newPost);
-    fs.writeFileSync('post.json', JSON.stringify(Data, null, 2));
-    res.redirect('/');
+   res.status(200).redirect("index.ejs")
 });
 
 app.listen(port, ()=>{
